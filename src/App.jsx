6928401a0fabd9485a4a6354bf742cf628d6cb5c
guilhermemadeira1 <161243338+guilhermemadeira1@users.noreply.github.com>
2025-11-12 22:16:46 -0300

    feat: rota dinamica para o perfil de usuario

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,16 @@ import Register from './routes/Register';
 import UserProfile from './routes/UserProfile';
 import ErrorRoute from './routes/ErrorRoute';
 import {Routes, Route, Navigate} from 'react-router-dom';
+import useAuth from './hooks/useAuth';
 
 export default function App(){
-  const authenticaded = true;
+  const {loggedInUser, isAuthenticaded} = useAuth();
   return(
     <Routes>
-        <Route path="/" element={authenticaded? <UserProfile/> : <Navigate to="/login"/>}/>
+        <Route path="/" element={isAuthenticaded? <Navigate to={`/usuario/${loggedInUser.id}`}/> : <Navigate to="/login"/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/registro" element={<Register/>}/>
-        <Route path="/perfil" element={authenticaded? <UserProfile/> : <Navigate to="/login"/>}/>
+        <Route path="/usuario/:id" element={isAuthenticaded? <UserProfile/> : <Navigate to="/login"/>}/>
         <Route path="*" element={<ErrorRoute/>}/>
     </Routes>
   );
