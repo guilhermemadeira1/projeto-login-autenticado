3b4a6cb35226c0451854aaebe9e7988e3bea12a9
guilhermemadeira1 <161243338+guilhermemadeira1@users.noreply.github.com>
2025-11-18 16:42:10 -0300

    feat: registro inserindo endereço da foto de perfil

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -9,9 +9,11 @@ import Paragraph from '../components/Paragraph';
 import NavLink from '../components/NavLink';
 
 export default function Register() {
-    const inputNameRef = useRef(null);
-    const inputEmailRef = useRef(null);
-    const inputPasswordRef = useRef(null);
+    const inputNameRef = useRef();
+    const inputEmailRef = useRef();
+    const inputPasswordRef = useRef();
+    const inputDescriptionRef = useRef();
+    const inputImageRef = useRef();
     const showMessage = useRef(false);
     const [regStatus, setRegStatus] = useState({success: false, message: ''});
 
@@ -20,6 +22,8 @@ export default function Register() {
         const name = inputNameRef.current.value;
         const email = inputEmailRef.current.value;
         const password = inputPasswordRef.current.value;
+        const description = inputDescriptionRef.current.value;
+        const image = inputImageRef.current.value;
         let users = JSON.parse(localStorage.getItem("registeredUsers")) || [];
 
         if(name && email && password){ 
@@ -34,7 +38,7 @@ export default function Register() {
             if(!emailBeingUsed){
                 if(!userDoesExist){
                     const nextId =  users.length > 0? Math.max(...users.map(u => u.id)) + 1 : 0; // mapeia os ids e faz um spread para separar o array em valores individuais para o Math.max comparar
-                    users = [...users, {id: nextId, name, email, password}];
+                    users = [...users, {id: nextId, name, email, password, description, image}];
                     localStorage.setItem("registeredUsers", JSON.stringify(users));
                     setRegStatus({success: true, message: "Usuário cadastrado com sucesso!"});
                    
@@ -46,7 +50,6 @@ export default function Register() {
             else{
                 setRegStatus({success: false, message: "Este email já está sendo usado"});
             }
-         
         }        
         else{
             setRegStatus({success: false, message: "Dados estão faltando"})
@@ -56,15 +59,21 @@ export default function Register() {
 
     return(
         <Background>
-            <Form action="">
+            <Form action="" $width="60%" $maxWidth="300px" $height="80%">
                 <div>
                     <Heading level='1'>Registre-se</Heading>
                     <Paragraph>Crie sua conta gratuita hoje</Paragraph>
                 </div>
                 <div style={{width: '100%'}}>
-                    <Field type="text" label="Nome" inputRef={inputNameRef}/>
-                    <Field type="email" label="Email" inputRef={inputEmailRef}/>
-                    <Field type="password" label="Senha" inputRef={inputPasswordRef}/>
+                    <div style={{display: 'flex', gap: '10px'}}>
+                        <Field type="text" label="Nome" inputRef={inputNameRef} required/>
+                        <Field type="email" label="Email" inputRef={inputEmailRef} required/>
+                    </div>
+                    <div style={{display: 'flex', gap: '10px'}}>
+                        <Field type="password" label="Senha" inputRef={inputPasswordRef} required/>
+                        <Field type="text" label="Imagem de perfil" inputRef={inputImageRef}/>
+                    </div>
+                    <Field type="textarea" label="Sobre mim" inputRef={inputDescriptionRef}/>
                 </div>
                 <Button submit onClick={(e) => handleRegister(e)}>Registrar</Button>
                 <Paragraph $color={regStatus.success? '#0a3' : '#f03'} $size="1em">
